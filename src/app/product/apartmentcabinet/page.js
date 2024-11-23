61326a6c48ff2ae5cd8fed8fdd4b409a3325b6c4
jodnddus <lockhost23@gmail.com>
2024-11-23 11:59:50 +0900

    asdf

--- src/app/product/apartmentcabinet/page.js
+++ src/app/product/apartmentcabinet/page.js
@@ -22,11 +22,21 @@ export default function ProductPage({params}) {
                 </h1>
                 <hr className="border-[1.5px] border-[#171717]"/>
                 <p className="desc font-pretendard text-[14px] tablet:text-2xl tablet:leading-9 desktop:text-[16px] desktop:leading-[140%] font-medium whitespace-pre-wrap">
+                    아파트는 다양한 것을 담는 일종의 캐비넷이라는 생각이 문득 들었습니다. 건물모양 캐비넷을 만들기로 하고, 디자인 단계에서 건물요소들을 가구요소로 해석해봤습니다.
+                    <br/>
+                    <br/>
+                    오른쪽 선반은 짧막한 책이나 화장품, 여러 잡동사니를 보관할 수 있고, 상단 서랍은 깊이가 깊어서 카메라, 헤드셋 같은 것들을 보관할 수 있습니다. 아래에는 짧막한 다리를 붙혀 평을 잡고, 동시에 필로티 구조를 묘사했습니다.
                 </p>
                 <b className={`desktop:text-[16px] desktop:leading-[140%] font-extrabold`}>cabinet, plywood, cherry
                     veneer, 24 summer </b>
                 <div
                     className="projects flex flex-row flex-wrap justify-center tablet:justify-start desktop:justify-start gap-[10px]">
+                    <Image
+                        src={images.apartmentcabinet[3]}
+                        alt={`apartmentcabinet-image-0`}
+                        placeholder="blur"
+                        className={`w-full`}
+                    />
                     <Image
                         src={images.apartmentcabinet[0]}
                         alt={`apartmentcabinet-image-0`}
@@ -41,12 +51,6 @@ export default function ProductPage({params}) {
                     />
                     <Image
                         src={images.apartmentcabinet[2]}
-                        alt={`apartmentcabinet-image-0`}
-                        placeholder="blur"
-                        className={`w-full desktop:w-[400px]`}
-                    />
-                    <Image
-                        src={images.apartmentcabinet[3]}
                         alt={`apartmentcabinet-image-0`}
                         placeholder="blur"
                         className={`w-full desktop:w-[400px]`}
